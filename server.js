15618a8b05fc46a29acc41b29b4533882d8c3751
ukuno <yukunos123@gmail.com>
2018-07-12 16:37:42 -0700

    add body parser and avatar is not required

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const post = require('./routes/api/post');
 
+//body parser 
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+//database connection
 const db = require('./config/key').mongoURI;
 
 app.get('/', (req, res) => res.send("hello world!!"));
@@ -14,8 +20,8 @@ mongoose.connect(db)
         .then(()=> console.log('db is connected'))
         .catch( err => console.log(err));
 
+        
 // app routers
-
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/post', post);
