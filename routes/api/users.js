4410d76888b78a463c8871971fa0d6e70a789c73
ukuno <yukunos123@gmail.com>
2018-07-12 17:35:23 -0700

    installed gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,34 @@
 const express =  require('express');
 const router = express.Router();
 
+//model of the user
+const User = require('../../models/user');
+
 //@route   GET api/users/test
 //@desc    test for users
 //@access  Public
 router.get('/test', (req, res) => res.json({ msg : 'this the users page'}));
+
+
+//@route   POST api/users/register
+//@desc    register
+//@access  Public
+
+router.post('/register', (req, res) => {
+    User.findOne({email : req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(400).json({email : 'Email already exists'});
+            } else {
+                const newUser = new User({
+                    name : req.body.name,
+                    email : req.body.email,
+                    password : req.body.password,
+                    avatar
+
+                });
+            }
+        })
+})
 
 module.exports = router;
