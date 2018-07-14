cbf50b29df965341b57ee4dfd5c0ef24429be80c
ukuno <yukunos123@gmail.com>
2018-07-13 17:53:50 -0700

    add register route to user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express =  require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 //model of the user
 const User = require('../../models/user');
@@ -20,13 +22,28 @@ router.post('/register', (req, res) => {
             if(user){
                 return res.status(400).json({email : 'Email already exists'});
             } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating
+                    d: 'mm' //default
+                });
+
                 const newUser = new User({
                     name : req.body.name,
                     email : req.body.email,
                     password : req.body.password,
                     avatar
-
                 });
+                bcrypt.genSalt(10,(err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                               .then(user => res.json(user))
+                               .catch(err => console.log(err));
+                    });
+                });
+
             }
         })
 })
